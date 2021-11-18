009629380558a45aa1fd32e54457c2943d0ce8ea
karl_probst <karlprobst@protonmail.com>
2021-11-18 14:28:30 -0300

    concertei o jogo, agora roda

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -17,12 +17,12 @@ const speed = 300
 
 
 	
-func _physics_process(delta: float) -> void:	
-	_get_input()
+func _physics_process(delta):	
+	_get_input(delta)
 	move_and_slide(velocity, UP)
 
 #THIS TAKES THE PLAYER INPUT
-func _get_input():
+func _get_input(delta):
 	var direction = -int(Input.is_action_pressed("Walk_Left")) + int(Input.is_action_pressed("Walk_Right"))
 	print(direction)
 	velocity.y += gravity * delta
